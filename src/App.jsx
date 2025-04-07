ffda7190f64053d19e04d448ea86ba8f23ac6640
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-07 11:19:30 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import Header from './components/Header'
+import Countdown from './components/Countdown'
+import './App.css'
+
+function App() {
+  return (
+     <div>
+      <Header/>
+      <Countdown/>
+     </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){ //처음은 항상 대문자로 작성
+    return(
+        <header>
+            <h1>이미림 & 김미림</h1>
+            <p>2025년 6월 15일 오후 1시</p>
+            <p>그랜드 호텔 서울</p>
+        </header>
+    )
+}
+export default Header;
